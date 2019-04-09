8867e21ef693e9cda6e4ca4d99cc94fdcc63588d
Lawrence Nahum <lawrence@greenaddress.it>
2019-04-09 15:43:48 +0200

    only synchronize the difference, randomize the list, fix too big tx issue, run concurrently

--- contrib/sync-mempool.sh
+++ contrib/sync-mempool.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+set -e
 
 source="$1"
 dest="$2"
 
-curl -s $source/mempool/txids | jq -r .[] | while read txid; do
+curl -s $source/mempool/txids | jq -r .[] | sort - > tx_ids_src.txt
+curl -s $dest/mempool/txids | jq -r .[] | sort - > tx_ids_dest.txt
+
+wc -l tx_ids_src.txt
+wc -l tx_ids_dest.txt
+
+comm -23 tx_ids_src.txt tx_ids_dest.txt > tx_ids_res.txt
+
+wc -l tx_ids_res.txt
+
+shuf tx_ids_res.txt > tx_ids_rnd.txt
+
+cat tx_ids_rnd.txt | while read txid; do
   echo pushing $txid
-  curl -s $dest/tx -d $(curl -s $source/tx/$txid/hex)
+  curl -s $dest/tx -d @- & <<TXHEX
+$(curl -s $source/tx/$txid/hex)
+TXHEX
+  pids[${i}]=$!
   echo
 done
+
+for pid in ${pids[*]}; do
+  wait $pid
+done
+
+echo "All done"
